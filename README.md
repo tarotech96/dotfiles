6a9ce11a2feb66e04acd279e82d2a9652dd1819f
tarotech96 <nguyenvancong4696@gmail.com>
2022-08-12 13:57:17 +0900

    add nvim-tree like nerdtree

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Guideline setup NeoVim + Tmux using Packer as Setup NeoVim + Tmux using Parker as plugin/package management
+# Guideline setup NeoVim + Tmux using Packer as plugin/package management and Lua language
 
 ## Pre install
 Install **brew** on your Mac.
@@ -10,6 +10,10 @@ And then install some necessary packages
 Nodejs
 ```
  brew install node
+```
+Git
+```
+ brew install git
 ```
 NeoVim
 ```
@@ -63,5 +67,33 @@ Install
 Install plugin
 ```
  :PackerInstall
+
 ```
 
+## Setup
+To use my configuration 
+```
+ git clone {repo_name}
+```
+```
+ cd ~/.config && mdkir nvim
+```
+Copy the configurations to nvim folder
+```
+ mv ~/dotfiles ~/.config/nvim
+```
+
+## Result
+**Autocomple**
+
+<img width="1190" alt="Screen Shot 2022-08-11 at 17 54 00" src="https://user-images.githubusercontent.com/44078555/184098251-6ecf4eea-33fc-4c64-be58-b5506b918721.png">
+
+**Tailwind CSS IntelliSense**
+
+<img width="1198" alt="Screen Shot 2022-08-11 at 17 55 23" src="https://user-images.githubusercontent.com/44078555/184098688-4210b412-bf74-4eea-acfe-8990404af660.png">
+
+**Search with shorcut `;f` or `;r` or `;sf`**
+
+<img width="1193" alt="Screen Shot 2022-08-11 at 17 57 25" src="https://user-images.githubusercontent.com/44078555/184099100-167b4a09-4ba9-4c38-87fd-a93318f033b7.png">
+
+and more...🥳
--- after/plugin/nvim-tree.rc.lua
+++ after/plugin/nvim-tree.rc.lua
@@ -12,10 +12,68 @@ nvim_tree.setup({
     }
   },
   renderer = {
-    group_empty = true
+    group_empty = false,
+    add_trailing = false,
+    highlight_git = false,
+    full_name = false,
+    highlight_opened_files = "none",
+    root_folder_modifier = ":~",
+    indent_markers = {
+      enable = false,
+      inline_arrows = true,
+      icons = {
+        corner = "└",
+        edge = "│",
+        item = "│",
+        none = " ",
+      }
+    },
+    icons = {
+      webdev_colors = true,
+      git_placement = "before",
+      padding = " ",
+      symlink_arrow = " ➛ ",
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        bookmark = "",
+        folder = {
+          arrow_closed = "",
+          arrow_open = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "✗",
+          staged = "✓",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "★",
+          deleted = "",
+          ignored = "◌",
+        },
+      }
+    },
+    special_files = { "Cargo.toml", "Makefile", "README.md", "readme.md" },
+    symlink_destination = true,
   },
   filters = {
-    dotfiles = true,
-    custom = { "^.git$" }
+    dotfiles = false,
+    custom = {},
+    exclude = {}
+  },
+  filesystem_watchers = {
+    enable = true,
+    debounce_delay = 50
   }
 })
